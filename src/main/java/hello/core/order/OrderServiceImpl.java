77cdf281b2695b109f61845078e0bf6e4379a3ee
GraceNam <zeidgame@gmail.com>
2022-03-08 20:20:19 +0900

    docs : 생성자 주입을 선택하라
    <description>
    * 생성자 주입의 장점
    - 불변
    >> 객체 주입 시 단 한번만 호출됨
    - 누락
    >> 수정자 주입의 경우 테스트 코드에서 누락되는 경우가 있음.
    - final
    >> 생성자 주입에서만 사용가능
    
    * 테스트 코드 작성
    - 수정자 주입 테스트코드 확인
    >> 누락으로 인해 NullPointerException 발생
    <issue-number>

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,10 +10,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-  private MemberRepository memberRepository;
-  private DiscountPolicy discountPolicy;
+  private final MemberRepository memberRepository;
+  private final DiscountPolicy discountPolicy;
 
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+  // 수정자 주입
+/*  @Autowired
+  public void setMemberRepository(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+  @Autowired
+  public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+    this.discountPolicy = discountPolicy;
+  }*/
+
+  // 셍성자 주입
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
   }
